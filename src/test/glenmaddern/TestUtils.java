b2d4c27a07541f41671310ac3816802dc3368ff6
dev <dev@mother.leapstream.rocks>
2009-02-06 15:34:40 +1000

    added some exception checking to Parser

--- src/test/glenmaddern/TestUtils.java
+++ src/test/glenmaddern/TestUtils.java
@@ -1,11 +1,15 @@
 package glenmaddern;
 
-import com.googlecode.instinct.internal.util.Reflector;
 import static com.googlecode.instinct.expect.Expect.expect;
+import com.googlecode.instinct.internal.expect.state.matcher.AssertException;
+import com.googlecode.instinct.internal.expect.state.matcher.AssertExceptionImpl;
+import com.googlecode.instinct.internal.util.Reflector;
 
 import java.lang.reflect.Method;
 
 public final class TestUtils {
+    public static final AssertException ASSERT_EXCEPTION = new AssertExceptionImpl();
+
     public static <T> void expectEqualsHashCodeAndToStringAreDefinedInClass(final Class<T> cls) {
         final Method equalsMethod = Reflector.getMethod(cls, "equals", Object.class);
         expect.that(equalsMethod.getDeclaringClass().equals(cls)).isTrue();
@@ -13,5 +17,9 @@ public final class TestUtils {
         expect.that(hashCodeMethod.getDeclaringClass().equals(cls)).isTrue();
         final Method toStringMethod = Reflector.getMethod(cls, "toString");
         expect.that(toStringMethod.getDeclaringClass().equals(cls)).isTrue();
+    }
+
+    public static <T> void assertException(final Class<T> exception, final String message, final Runnable block) {
+        ASSERT_EXCEPTION.assertThrows(exception, message, block);
     }
 }
--- src/test/glenmaddern/gmfs/ParserContext.java
+++ src/test/glenmaddern/gmfs/ParserContext.java
@@ -3,10 +3,10 @@ package glenmaddern.gmfs;
 import static com.googlecode.instinct.expect.Expect.expect;
 import com.googlecode.instinct.integrate.junit4.InstinctRunner;
 import com.googlecode.instinct.marker.annotate.Specification;
-import com.googlecode.instinct.internal.expect.state.matcher.AssertExceptionImpl;
+import static glenmaddern.TestUtils.assertException;
+import static glenmaddern.gmfs.Converters.STRING_TO_BOOL;
 import static glenmaddern.gmfs.Converters.STRING_TO_DOUBLE;
 import static glenmaddern.gmfs.Converters.STRING_TO_INT;
-import static glenmaddern.gmfs.Converters.STRING_TO_BOOL;
 import static glenmaddern.gmfs.T.t;
 import org.junit.runner.RunWith;
 
@@ -45,11 +45,26 @@ public final class ParserContext {
 
     @Specification
     public void shouldFailWithIncorrectLengthTokens() {
-        new AssertExceptionImpl().assertThrows(IllegalArgumentException.class, "Expected list of length 3, got 2", new Runnable() {
+        expectFailToParse3("1,False");
+        expectFailToParse3("1,False,3.0,2");
+        expectFailToParse2("1,False,3.0");
+        expectFailToParse2("1");
+    }
+
+    private void expectFailToParse3(final String s) {
+        final List<String> strings = asList(s.split(","));
+        assertException(IllegalArgumentException.class, "Expected list of length 3, got " + strings.size(), new Runnable() {
             public void run() {
-                final String line = "1,False";
-                final List<String> strings = asList(line.split(","));
-                final T3<Integer, Boolean, Double> parsedWithBoolean = Parser.parse(strings, t(STRING_TO_INT, STRING_TO_BOOL, STRING_TO_DOUBLE));
+                Parser.parse(strings, t(STRING_TO_INT, STRING_TO_BOOL, STRING_TO_DOUBLE));
+            }
+        });
+    }
+
+    private void expectFailToParse2(final String s) {
+        final List<String> strings = asList(s.split(","));
+        assertException(IllegalArgumentException.class, "Expected list of length 2, got " + strings.size(), new Runnable() {
+            public void run() {
+                Parser.parse(strings, t(STRING_TO_INT, STRING_TO_BOOL));
             }
         });
     }
