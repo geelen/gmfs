91909799ae6c75f36f7f76ab8265073d56ca19f8
dev <dev@mother.leapstream.rocks>
2009-02-06 13:27:24 +1000

    got git integration woo! also pair passes now

--- src/core/glenmaddern/gmfs/Pair.java
+++ src/core/glenmaddern/gmfs/Pair.java
@@ -1,27 +1,40 @@
 package glenmaddern.gmfs;
 
 public final class Pair<A> {
-    private A first;
-    private A second;
+    private final T2<A, A> t2;
 
-    private Pair(final A first, final A second) {
-        this.first = first;
-        this.second = second;
+    private Pair(final T2<A, A> t2) {
+        this.t2 = t2;
     }
 
     public A getFirst() {
-        return first;
+        return t2._1();
     }
 
     public A getSecond() {
-        return second;
+        return t2._2();
     }
 
     public <B> Pair<B> map(final F<A, B> f) {
-        return new Pair<B>(f.f(first), f.f(second));
+        return Pair.pair(f.f(t2._1()), f.f(t2._2()));
     }
 
-    public static <T> Pair<T> pair(final T a, final T b) {
-        return new Pair<T>(a, b);
+    public static <A> Pair<A> pair(final A a, final A b) {
+        return new Pair<A>(T.t(a, b));
+    }
+
+    public String toString() {
+        return t2.toString();
+    }
+
+    public int hashCode() {
+        return t2.hashCode();
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return t2.equals(pair.t2);
     }
 }
--- src/test/glenmaddern/TestUtils.java
+++ src/test/glenmaddern/TestUtils.java
@@ -1,16 +1,17 @@
 package glenmaddern;
 
 import com.googlecode.instinct.internal.util.Reflector;
+import static com.googlecode.instinct.expect.Expect.expect;
 
 import java.lang.reflect.Method;
 
 public final class TestUtils {
     public static <T> void expectEqualsHashCodeAndToStringAreDefinedInClass(final Class<T> cls) {
         final Method equalsMethod = Reflector.getMethod(cls, "equals", Object.class);
-        com.googlecode.instinct.expect.Expect.expect.that(equalsMethod.getDeclaringClass().equals(cls)).isTrue();
+        expect.that(equalsMethod.getDeclaringClass().equals(cls)).isTrue();
         final Method hashCodeMethod = Reflector.getMethod(cls, "hashCode");
-        com.googlecode.instinct.expect.Expect.expect.that(hashCodeMethod.getDeclaringClass().equals(cls)).isTrue();
+        expect.that(hashCodeMethod.getDeclaringClass().equals(cls)).isTrue();
         final Method toStringMethod = Reflector.getMethod(cls, "toString");
-        com.googlecode.instinct.expect.Expect.expect.that(toStringMethod.getDeclaringClass().equals(cls)).isTrue();
+        expect.that(toStringMethod.getDeclaringClass().equals(cls)).isTrue();
     }
 }
